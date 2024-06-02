910c50092586dd77445e9bd84e5589b2a0ca0fbf
alexa <alexander.55.fc@gmail.com>
2024-06-02 21:08:02 +0200

    Add SpringBoot proyect from Spring Initializr

---
+++ src/main/java/com/gym/tfg/GymTfgApplication.java
@@ -0,0 +1,13 @@
+package com.gym.tfg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymTfgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymTfgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gym/tfg/GymTfgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gym.tfg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymTfgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
