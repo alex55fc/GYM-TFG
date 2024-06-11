3bea5f2c27a581c0101fc8834c48db94a3ab5286
Alexander Fuela <ALEXANDERPAUL.FUELA@VIEWNEXT.COM>
2024-06-11 09:43:18 +0200

    New inserts in data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,10 @@
--- Inserta las suscripciones básicas y premium en la tabla de suscripciones
+-- Insert basic and premium subscriptions into the table
 INSERT INTO subscriptions (subscription_name, monthly_price, subscription_type) VALUES ('BasicSubscription', 20.00, 'BASIC');
-INSERT INTO subscriptions (subscription_name, monthly_price, subscription_type) VALUES ('PremiumSubscription', 50.00, 'PREMIUM');
+INSERT INTO subscriptions (subscription_name, monthly_price, subscription_type) VALUES ('PremiumSubscription', 40.00, 'PREMIUM');
+
+-- User with BasicSubscription
+INSERT INTO users (email, password, name, surname, age, weight, gender, subscription_id) VALUES ('user1@gmail.com', 'password1', 'John', 'Doe', 33, 75.5, 'MALE', 1);
+-- User with PremiumSubscription
+INSERT INTO users (email, password, name, surname, age, weight, gender, subscription_id) VALUES ('user2@gmail.com', 'password2', 'Jane', 'Smith', 25, 65.0, 'FEMALE', 2);
+
 
