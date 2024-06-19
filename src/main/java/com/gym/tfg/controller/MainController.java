557d920ef6e331a2ba3814199823a8a948827f97
Alexander Fuela <ALEXANDERPAUL.FUELA@VIEWNEXT.COM>
2024-06-19 07:13:57 +0200

    almost done css implements in the proyect

--- src/main/java/com/gym/tfg/controller/MainController.java
+++ src/main/java/com/gym/tfg/controller/MainController.java
@@ -3,8 +3,6 @@ package com.gym.tfg.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import jakarta.servlet.http.HttpSession;
-
 /**
  * MainController handles requests for the main pages of the application.
  */
